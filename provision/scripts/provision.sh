97c0af4c787d5fe6d9c5ba3d2db7abc0a4c5e519
吉島司 <yoshijima0712@yoshijimatsukasa-no-MacBook-Air.local>
2016-12-29 17:06:52 +0900

    add new file

---
+++ provision/scripts/provision.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+### phpenv
+curl -L https://raw.github.com/CHH/phpenv/master/bin/phpenv-install.sh | bash
+git clone git://github.com/php-build/php-build.git ~/.phpenv/plugins/php-build
+echo 'export PATH=$PATH:$HOME/.phpenv/bin' >> ~/.bash_profile
+echo 'eval "$(phpenv init -)"' >>  ~/.bash_profile
+source ~/.bash_profile
+
+### php7.0.0
+phpenv install 7.0.0
+phpenv rehash
+phpenv global 7.0.0
+
+### php-fpm
+cp -a php-fpm.conf ~/.phpenv/versions/7.0.0/etc/php-fpm.conf
+cp -a www.conf ~/.phpenv/versions/7.0.0/etc/php-fpm.d/www.conf
+
+sudo /etc/rc.d/init.d/php-fpm start
+sudo chkconfig php-fpm on
---
+++ provision/scripts/provision_root.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+### yum update
+yum -y update
+
+### NTP
+yum -y install ntp
+# cp -a /vagrant/ntp.conf /etc/ntp.conf
+/sbin/service ntpd restart
+/sbin/chkconfig ntpd on
+
+### timezone
+cp -p /usr/share/zoneinfo/Japan /etc/localtime
+
+### iptables off
+/sbin/iptables -F
+/sbin/service iptables stop
+/sbin/chkconfig iptables off
+
+### nginx for centos6
+rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
+yum -y install nginx
+cp -a app.conf /etc/nginx/conf.d/app.conf
+/sbin/service nginx restart
+/sbin/chkconfig nginx on
+
+### git
+yum -y install git
+
+### wget
+yum -y install wget
+sed -i -e "s/enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+yum -y install gcc libxml2-devel bison bison-devel openssl-devel curl-devel libjpeg-devel libpng-devel readline-devel libxslt-devel autoconf automake patch
+yum --enablerepo=epel -y install libmcrypt-devel libtidy-devel re2c
+
+### php-fpm
+cp -a php-fpm /etc/init.d/php-fpm
